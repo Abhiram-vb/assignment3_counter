e47123abd14eb0a728f41994e0c2a9adfa4f3e4f
Abhiram <pabhi2000@gmail.com>
2021-09-14 08:31:14 +0530

    added html,css,js files

---
+++ script.js
@@ -0,0 +1,16 @@
+let incBtnEl = document.getElementById('incBtn')
+let decBtnEl = document.getElementById("decBtn")
+let resetBtnEl = document.getElementById("resetBtn")
+let counterEl = document.getElementById("counter")
+
+incBtnEl.onclick = function() {
+    let counterVal = parseInt(counterEl.textContent)
+    counterEl.textContent = counterVal + 1
+}
+decBtnEl.onclick = function() {
+    let counterVal = parseInt(counterEl.textContent)
+    counterEl.textContent = counterVal - 1
+}
+resetBtnEl.onclick = function() {
+    counterEl.textContent = 0
+}
