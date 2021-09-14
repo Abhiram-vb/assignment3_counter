db7c07188f3afa965f8c95592cc6c9749d74e34f
Abhiram-vb <90171255+Abhiram-vb@users.noreply.github.com>
2021-09-14 09:27:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ let incBtnEl = document.getElementById('incBtn')
 let decBtnEl = document.getElementById("decBtn")
 let resetBtnEl = document.getElementById("resetBtn")
 let counterEl = document.getElementById("counter")
+let paraEl = document.getElementById("para")
 
 incBtnEl.onclick = function() {
     let counterVal = parseInt(counterEl.textContent)
     counterEl.textContent = counterVal + 1
+    paraEl.textContent = ""
 }
 decBtnEl.onclick = function() {
     let counterVal = parseInt(counterEl.textContent)
     if (counterVal > 0) {
         counterEl.textContent = counterVal - 1
-    } 
+        paraEl.textContent = ""
+    } else {
+        paraEl.textContent = "No negative counter"
+    }
 }
 resetBtnEl.onclick = function() {
     counterEl.textContent = 0
+    paraEl.textContent = ""
 }
